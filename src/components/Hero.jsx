0dd6950339cb1b00ff3d7855cab117e507e6c3ce
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-09-10 15:46:56 +0200

    Restructure styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import '../styles/hero.scss';
 
 const Hero = () => (
-  <>
+  <div className="blue-theme full-width">
     <h1 className="hello">
       Hi, my name is&nbsp;
-      <span className="name">
+      <span className="accent">
         Lucas Forster
       </span>
       .
     </h1>
-  </>
+  </div>
 );
 
 export default Hero;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Hero from '../components/Hero';
 
-import '../styles/index.scss';
+import '../styles/main.scss';
 
 const Index = () => (
   <div className="index">
