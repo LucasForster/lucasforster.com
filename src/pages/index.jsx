859f92e6353b6801694bdba1e9740d1d8db6a47d
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-09-10 11:12:24 +0200

    Clean index and 404 page

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import '../styles/index.scss';
+
+const Index = () => (
+  <div className="index">
+    <h1>lucasforster.com</h1>
+  </div>
+);
+
+export default Index;
