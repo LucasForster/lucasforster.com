dd23585b49e708da61c95a7d6bc6bf64b76abff8
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-09-10 12:33:42 +0200

    Setup project (#1)
    * Set site url
    
    * Clean readme
    
    * Clean index and 404 page
    
    * Add colors
    
    * Add VS Code settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.lucasforster.com",
     title: "lucasforster.com",
   },
   plugins: ["gatsby-plugin-sass"],
