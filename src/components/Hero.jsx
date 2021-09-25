c8af632cfa9fc2577e7c079373ca737fd8f57694
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-09-25 17:58:38 +0200

    Call translation function

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const Hero = () => (
-  <div className="blue-theme full-height">
-    <h1 className="fade-in-left">
-      Hi, my name is&nbsp;
-      <span className="accent">
-        Lucas Forster
-      </span>
-      .
-      <br/>
-      I'm a developer available for hire.
-    </h1>
-    <a className="button reveal-delayed" href="mailto:mail@lucasforster.com">
-      Contact
-    </a>
-  </div>
-);
+const Hero = () => {
+
+  const { t } = useTranslation();
+
+  return (
+    <div className="blue-theme full-height">
+      <h1 className="fade-in-left">
+        {t("Hi, my name is")}&nbsp;
+        <span className="accent">
+          Lucas Forster
+        </span>
+        .
+        <br/>
+        {t("I'm a developer available for hire.")}
+      </h1>
+      <a className="button reveal-delayed" href="mailto:mail@lucasforster.com">
+        Contact
+      </a>
+    </div>
+  );
+};
 
 export default Hero;
