243696678bba3591b83c4b101ce07dbaa70e3ddd
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-09-30 14:04:09 +0200

    Move type declaration

--- src/@types/react-i18next.d.ts
+++ src/@types/react-i18next.d.ts
@@ -1,6 +1,6 @@
 import "react-i18next";
 
-import { defaultNS, ResourcesType } from "../src/model/i18next";
+import { defaultNS, ResourcesType } from "../model/i18next";
 
 declare module "react-i18next" {
   interface CustomTypeOptions {
