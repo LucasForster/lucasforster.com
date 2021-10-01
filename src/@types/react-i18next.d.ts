c8807f3c1e40f54ff9f6f4e73f914b4b9e6b66e5
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-10-01 17:01:27 +0200

    Fix outdated imports

--- src/@types/react-i18next.d.ts
+++ src/@types/react-i18next.d.ts
@@ -1,6 +1,6 @@
 import "react-i18next";
 
-import { defaultNS, ResourcesType } from "../model/i18next";
+import { defaultNS, ResourcesType } from "../model/i18next.config";
 
 declare module "react-i18next" {
   interface CustomTypeOptions {
